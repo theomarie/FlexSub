e768c419b1f15a4dfddd576e7f2c820e32729461
myriamdesporte <myriam.desporte@gmail.com>
2024-10-23 16:13:52 +0200

    Vue Test

---
+++ FlexSub/TestView.swift
@@ -0,0 +1,19 @@
+//
+//  TestView.swift
+//  FlexSub
+//
+//  Created by Apprenant 108 on 23/10/2024.
+//
+
+import SwiftUI
+
+struct TestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Travail de My")
+    }
+}
+
+#Preview {
+    TestView()
+}
