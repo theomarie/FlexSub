3213de418b8c07e2bf404aad8019e7185ebb9d64
theomarie <theomarie@protonmail.com>
2024-10-23 13:43:14 +0200

    Initial Commit

---
+++ FlexSub/FlexSubApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlexSubApp.swift
+//  FlexSub
+//
+//  Created by Theo Marie on 23/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct FlexSubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
